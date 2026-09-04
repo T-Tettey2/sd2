0687fe56901dd5202434d33dd86a5fbe75dbf679
T Tettey <tjaytettey02@gmail.com>
2026-09-04 14:38:54 +0100

    Add dynamic routes: hello/:name, user/:id, student/:name/:id

--- app.js
+++ app.js
@@ -14,3 +14,15 @@ app.get('/roehampton', function (req, res) {
   console.log(req.url);
   res.send('hello roehampton');
 });
+app.get('/hello/:name', function (req, res) {
+  console.log(req.params);
+  res.send('hello ' + req.params.name);
+});
+
+app.get('/user/:id', function (req, res) {
+  res.send('User ID: ' + req.params.id);
+});
+
+app.get('/student/:name/:id', function (req, res) {
+  res.send('Student Name: ' + req.params.name + ', Student ID: ' + req.params.id);
+});
